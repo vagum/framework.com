b60f1c9afc6021a2691c33b31452777f95ebc719
vagum <marika@bakzon.com>
2024-09-21 02:28:41 +0300

    Autowiring Part2

--- framework/src/Container/Container.php
+++ framework/src/Container/Container.php
@@ -37,4 +37,31 @@ class Container implements ContainerInterface
     {
         return array_key_exists($id, $this->services);
     }
+
+    private function resolve($class)
+    {
+        // 1. Создать экземпляр класса Reflection
+        $reflectionClass = new \ReflectionClass($class);
+
+        // 2. Использовать Reflection для попытки получить конструктор класса
+        $constructor = $reflectionClass->getConstructor();
+
+        // 3. Если нет конструктора, просто создавать экземпляр
+        if (is_null($constructor)) {
+            return $reflectionClass->newInstance();
+        }
+
+        // 4. Получить параметры конструктора
+        $constructorParams = $constructor->getParameters();
+
+        // 5. Получить зависимости
+        $classDependencies = $this->resolveClassDependencies($constructorParams);
+
+        // 6. Создать экземпляр с зависимостями
+        $instance = $reflectionClass->newInstanceArgs($classDependencies);
+
+        // 7. Вернуть объект
+        return $instance;
+
+    }
 }
