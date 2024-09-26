af38ba93e978ec7f2781edd7cd68bde577131cda
vagum <marika@bakzon.com>
2024-09-26 15:30:51 +0300

    PSR-15 Middleware Fix

--- framework/src/Http/Middleware/Success.php
+++ framework/src/Http/Middleware/Success.php
@@ -5,9 +5,9 @@ namespace Somecode\Framework\Http\Middleware;
 use Somecode\Framework\Http\Request;
 use Somecode\Framework\Http\Response;
 
-class Success implements RequestHandlerInterface
+class Success implements MiddlewareInterface
 {
-    public function handle(Request $request): Response
+    public function process(Request $request, RequestHandlerInterface $handler): Response
     {
         return new Response('Hello World!');
     }
